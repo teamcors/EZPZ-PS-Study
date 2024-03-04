949ccbc58eef90fbb1d600c82d9858d83edaf7e7
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-03-04 16:58:50 +0900

    백준 DFS와 BFS 476ms

---
+++ boj/1260/DFS와 BFS_chaeeun-Han.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+    
+visited = [False] * (n + 1)
+dfs(graph, v, visited)
+print()
+
+visited = [False] * (n + 1)
+bfs(graph, v, visited)
