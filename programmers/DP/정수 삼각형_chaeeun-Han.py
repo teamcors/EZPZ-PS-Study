19e38a5f0690be325f3535baac7e05dc780f5aa7
ChaeEun Han <68267916+chaeeun-Han@users.noreply.github.com>
2024-03-31 21:28:57 +0900

    프로그래머스 정수 삼각형 AC

---
+++ programmers/DP/정수 삼각형_chaeeun-Han.py
@@ -0,0 +1,16 @@
+# 45min 26sec
+
+# 물줄기가 흐르듯. 위에서 아래로 내려가기
+# 계산한 값으로 대체 후 불러오기 (반복되는 연산 최소화)
+
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:	# 왼쪽 끝이면
+                triangle[i][j] += triangle[i-1][0]  # 이전 층의 0번째 값 더하기
+            elif j == len(triangle[i])-1:	# 오른쪽 끝이면
+                triangle[i][j] += triangle[i-1][-1]	# 이전 층의 -1번째 값 더하기
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+
+    return max(triangle[-1])
