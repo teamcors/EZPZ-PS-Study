b64e74f2f11415b29cc5d24f2f7b7efd8fc17d1b
Queue-ri <qriositylog@gmail.com>
2024-09-28 22:24:13 +0900

    백준 부분수열의 합 240ms

--- boj/1182/부분수열의 합_Queue-ri.py
+++ boj/1182/부분수열의 합_Queue-ri.py
@@ -3,18 +3,18 @@ from itertools import combinations
 input = sys.stdin.readline
 
 def solution():
-    n, s = map(int,input().split())
+    n, s = map(int, input().split())
     seq = list(map(int, input().split()))
     
-    cnt = 0
+    ans = 0
     for size in range(1, len(seq)+1):
         sums = [sum(x) for x in combinations(seq, size)]
         for k in sums:
             if k == s:
-                cnt += 1
-            
-    print(cnt)
-    
+                ans += 1
+                
+    print(ans)
+        
     
 if __name__ == '__main__':
     solution()
